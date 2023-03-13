cf4f55c593ea90f523337281b0e352ee333257e2
Nhuengzii <32nhueng@gmail.com>
2023-03-13 15:02:34 +0700

    Add fonts and edit bootstrap

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,3 +1,5 @@
-for i in polybar ranger picom kitty;do
+for i in polybar ranger picom kitty dunst;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
 done
+sudo ln -s ~/.config/herbstluftwm/fonts/cascadiacode /usr/share/fonts/
+fc-cache -fv
