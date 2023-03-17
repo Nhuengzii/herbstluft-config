42d98e0930913d0a57298356a6606fb37936f7ce
Nhuengzii <32nhueng@gmail.com>
2023-03-17 13:28:14 +0700

    Save state of nitrogen

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,4 +1,4 @@
-for i in polybar ranger picom kitty dunst;do
+for i in polybar ranger picom kitty dunst nitrogen;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
 done
 sudo ln -s ~/.config/herbstluftwm/fonts/cascadiacode /usr/share/fonts/
