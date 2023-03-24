e79a5bc31757a56e5fd774ee693e093c4f67620a
Nhuengzii <32nhueng@gmail.com>
2023-03-24 23:02:26 +0700

    Astolfo :)

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,5 +1,5 @@
 for i in polybar ranger picom kitty dunst nitrogen;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
 done
-sudo ln -s ~/.config/herbstluftwm/fonts/cascadiacode /usr/share/fonts/
+sudo cp -r ~/.config/herbstluftwm/fonts/cascadiacode /usr/share/fonts/
 fc-cache -fv
