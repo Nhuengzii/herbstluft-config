9c945c2714fe55047e5c5235e3b6d9d4bf43e1d1
Nhuengzii <32nhueng@gmail.com>
2023-03-26 16:31:08 +0700

    add neovim setup to bootstrap.sh

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,4 +1,5 @@
-sudo pacman -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm
+sudo pacman -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm neovim
+git clone https://github.com/Nhuengzii/neovim-config.git ~/.config/nvim
 for i in polybar ranger picom kitty dunst nitrogen;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
 done
