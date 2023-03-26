edd2f04bc1c2dd9a58402af42f791729e2ddf27b
Nhuengzii <32nhueng@gmail.com>
2023-03-26 17:22:10 +0700

    add sddm setup to bootstrap

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,4 +1,7 @@
-sudo pacman -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm neovim ueberzug
+yay -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm neovim ueberzug sddm-sugar-candy-git
+sudo rm -rf /usr/share/sddm/themes/sugar-candy
+sudo cp -r ~/.config/herbstluftwm/sddm/sugar-candy /usr/share/sddm/themes
+sudo cp -r ~/.config/herbstluftwm/sddm/sddm.conf /etc/
 git clone https://github.com/Nhuengzii/neovim-config.git ~/.config/nvim
 for i in polybar ranger picom kitty dunst nitrogen;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
