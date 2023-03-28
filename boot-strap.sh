855645b5d468c0630cc0645ed9ae988f1569244f
Nhuengzii <32nhueng@gmail.com>
2023-03-28 18:16:44 +0700

    remove sddm and install docker as setup

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,7 +1,7 @@
-yay -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm neovim ueberzug sddm-sugar-candy-git
-sudo rm -rf /usr/share/sddm/themes/sugar-candy
-sudo cp -r ~/.config/herbstluftwm/sddm/sugar-candy /usr/share/sddm/themes
-sudo cp -r ~/.config/herbstluftwm/sddm/sddm.conf /etc/
+yay -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm neovim ueberzug ly docker --noconfirm
+sudo systemctl enable ly
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
 git clone https://github.com/Nhuengzii/neovim-config.git ~/.config/nvim
 for i in polybar ranger picom kitty dunst nitrogen;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
