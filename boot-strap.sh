8cf21be0ba246b672b6680c94b146851d183de46
Nhuengzii <32nhueng@gmail.com>
2023-04-05 13:05:30 +0700

    update boot-strap

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,12 +1,12 @@
-yay -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen spectacle nodejs npm neovim ueberzug ly docker brightnessctl --noconfirm
-sudo systemctl enable ly
+yay -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen nodejs npm neovim ueberzug docker brightnessctl go github-cli zsh --noconfirm
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
-git clone https://github.com/Nhuengzii/neovim-config.git ~/.config/nvim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone https://github.com/Nhuengzii/astronvim-config.git ~/.config/nvim/lua/user
 for i in polybar ranger picom kitty dunst nitrogen rofi;do
     ln -s ~/.config/herbstluftwm/$i ~/.config
 done
 sudo cp -r ~/.config/herbstluftwm/fonts/cascadiacode /usr/share/fonts/
 fc-cache -fv
-git config --global user.emai "32nhueng@gmail.com"
+git config --global user.email "32nhueng@gmail.com"
 git config --global user.name "Nhuengzii"
