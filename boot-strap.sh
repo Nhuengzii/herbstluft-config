89161182c9e269a4b529b7e30f1ae70a2a30eb36
Nhuengzii <32nhueng@gmail.com>
2023-04-29 19:52:12 +0700

    basic xubuntu change

--- boot-strap.sh
+++ boot-strap.sh
@@ -1,6 +1,24 @@
-yay -S polybar ranger picom kitty herbstluftwm rofi dunst nitrogen nodejs npm neovim ueberzug docker brightnessctl go github-cli zsh cargo lazygit noto-fonts-emoji --noconfirm
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
+sujdo apt install polybar ranger picom kitty herbstluftwm rofi dunst nitrogen nodejs npm ueberzug brightnessctl gh zsh -y
+
+# Docker Seup
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
+sudo chmod a+r /etc/apt/keyrings/docker.gpg 
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update &&
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &&
+sudo usermod -aG docker $USER &&
+
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 git clone https://github.com/Nhuengzii/astronvim-config.git ~/.config/nvim/lua/user
 for i in polybar ranger picom kitty dunst nitrogen rofi;do
@@ -9,5 +27,5 @@ done
 sudo cp -r ~/.config/herbstluftwm/fonts/cascadiacode /usr/share/fonts/
 fc-cache -fv
 git config --global user.email "32nhueng@gmail.com"
-git config --global user.name "Nhuengzii"
+git config --global user.name "Nhuengzii" 
 git config --global init.defaultBranch main
